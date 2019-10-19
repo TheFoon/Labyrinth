1ac8e6be4adb7e80e3c7516a45bb9e131e82c75f
DoM <lukacsdominik1@gmail.com>
2019-10-19 12:44:59 +0200

    first design elements

--- Labyrinth/Labyrinth/Form1.cs
+++ Labyrinth/Labyrinth/Form1.cs
@@ -16,5 +16,19 @@ namespace Labyrinth
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Panel titleScreen = new Panel();
+            titleScreen.Dock = DockStyle.Fill;
+            Label TitleLabel = new Label();
+            TitleLabel.Text = "Labyrinth";
+            titleScreen.Controls.Add(TitleLabel);
+            Button StartButton = new Button();
+            StartButton.Text = "Start Game";
+            StartButton.Click += (s, ev) => {};
+            titleScreen.Controls.Add(StartButton);
+            this.Controls.Add(titleScreen);
+        }
     }
 }
